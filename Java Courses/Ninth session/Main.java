4d0d2d2cb9217a3cbe543678184d0577db7fb0f7
yayademb97 <yayadembele09@gmail.com>
2025-02-23 20:19:21 +0100

    My Ninth Session based on Methods Concept In Java

---
+++ Java Courses/Ninth session/Main.java
@@ -0,0 +1,28 @@
+// package Java Courses.Ninth session;
+
+//! 📖♨️ Method ♨️📖:
+//! 🚀 Définition 🚀: Une méthode est une fonction en orienté-objet, un bloc de code qui exécute une tâche spécifique. 
+//!                    Elle permet de réutiliser du code, d'organiser le programme et de le rendre plus modulaire.
+
+//? 💬 Type de méthode en Java 💬:
+    //* - 1️⃣ Méthodes d'instance (nécessitent un objet pour être appelées)
+    //* - 2️⃣ Méthodes statiques (appartiennent à la classe et peuvent être appelées sans instance)
+    //* - 3️⃣ Méthodes avec retour et sans retour
+    //* - 4️⃣ Méthodes avec ou sans paramètres
+
+
+
+    
+
+//! 📝 Summary 📝: 
+//! Il est obligatoire de programmer en Java en Orienté-Objet, le programme minimale qu'on a est une classe dans notre exemple "Main",
+//* avec une méthode main. Par défaut lorsque nous allons exécuter notre programme, 
+//* la machine virtuelle Java (JVM) va chercher notre classe principale qui est la classe "Main", 
+//* ça veut dire qu'on a pas besoin de l'instancier nous meme, tout en cherchant notre méthode "main", qui est notre point d'entrée du programme.
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Java Courses/Ninth session/Method.java
@@ -0,0 +1,11 @@
+// package Java Courses.Ninth session;
+
+
+
+public class Method {
+    //! Constructor
+    public Method()
+    {
+
+    }
+}
