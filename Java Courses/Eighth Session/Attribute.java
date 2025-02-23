96960dab571dac36b8f076df3001af434792a76f
yayademb97 <yayadembele09@gmail.com>
2025-02-23 18:16:01 +0100

    Attribute learning Concepts

---
+++ Java Courses/Eighth Session/Attribute.java
@@ -0,0 +1,37 @@
+// package Java Courses.Eighth Session;
+
+
+
+
+public class Attribute {
+    //* Attribute Fields:
+    private String myName;
+    private int age;
+    //*  📌 Note 📌: Comme les attributs sont définis en dehors de toute méthode, 
+    //*               mais à l'intérieur de la classe, par defaut leur niveau d'accéssibilité serait "private"
+
+
+    //* 🔹Remarque: À partir de là, on va modifier le constructeur, comme on le sait on va lui donner des paramètres.
+    //*              Quand on va construire un objet de la classe Attribute, 
+    //*              on va lui spécifier que cet objet possède un nom "myName", un age "age", c'est l'instanciation de la classe.
+
+    //! 📌 Note 📌: Pour stocker ces informations je vais le stocker au niveau de mon objet, 
+    //*              ou de cette instance là pour ne pas perder mes informations.
+    //*              On va seuleent affecter aux attributs de la classe en modifiant le constructeur comme il a besoin dans notre cas
+    //*              d'un nom "myName", et de l'age "age".
+
+    //! Constructor
+    public Attribute(String p_myName, int p_age)
+    {
+        this.myName = p_myName;  //* initialisation de l'attribut myName */
+        this.age = p_age;        //* initialisation de l'attribut age */
+
+        //* 🔹Note: "this" le mot-clé qui fait référence à l'instance de la classe.
+
+        //* Verifions que les attributs sont bien enrégistrés:
+        System.out.println(this.myName + " " + this.age);
+
+
+        //? 📌 Note ⚠️: Le niveau d'accès"private" exprime que cet attribut n'est accéssible (lire, modifier, etc...) que dans la classe où elle a été définie.
+    }
+}
---
+++ Java Courses/Eighth Session/Main.java
@@ -0,0 +1,24 @@
+// package Java Courses.Eighth Session;
+
+// ! Attribute: En Java, le terme "attribut" fait généralement référence aux champs (variables d'instance ou variables de classe) d'une classe. 
+//! Les attributs stockent l'état ou les propriétés d'un objet.
+
+//! 📌 Note 📌: Quand elle est faite au sein d'une méthode, on parle d'une variable.
+//!              Par contre les attributs sont définis en dehors de la classe. 👉 (Attribute.java)
+
+
+public class Main {
+    public static void main(String[] args) {
+        //* Création d'instances de la classe "Attribute" dans la classe Main 
+        Attribute myAttribute1 = new Attribute("Yaya Dembele", 28);
+        System.out.println("Hello This is the information regarding my first abject created from Attribute class\n" + myAttribute1);
+        System.out.println("----------------------------------------------------------------------------");
+        Attribute myAttribute2 = new Attribute("Zeinab Diakité", 25);
+        System.out.println("Hello This is the information regarding my first abject created from Attribute class\n" + myAttribute2);
+
+        //* Exemple: Common Mistake
+        //* Si j'essaie d'accéder depuis la classe Main à l'attribut "p_myName" 
+        // System.out.println(myAttribute1.p_myName);  //? Output: Error: Puisque l'attribut est d"fini "private" , 
+                                                       //* donc du coup on ne peut pas l'accéder à une classe extérieure.
+    }
+}
